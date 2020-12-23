304a5f0fe90e4be8769fd09f275dc80c3c6e54f4
JustSnow <justsnow17@yandex.ru>
2020-12-23 13:06:25 +0300

    f

--- spec/lib/graph/algorithms/points_iterator_spec.rb
+++ spec/lib/graph/algorithms/points_iterator_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe MetaStruct::Graph::Algorithms::PointsIterator do
   let(:child_for_child_for_root) do
     MetaStruct::Graph::Node.create(labels: ['second level weight -1'])
   end
+  let(:child_2_for_child_for_root) do
+    MetaStruct::Graph::Node.create(labels: ['second level child 2'])
+  end
+  let(:child_3_for_child_for_root) do
+    MetaStruct::Graph::Node.create(labels: ['second level child 3'])
+  end
+  let(:child_for_child_3_for_child_for_root) do
+    MetaStruct::Graph::Node.create(labels: ['third level child 1'])
+  end
 
   let(:edge_for_root) do
     MetaStruct::Graph::Edge.create(
@@ -29,9 +38,49 @@ RSpec.describe MetaStruct::Graph::Algorithms::PointsIterator do
       weight: -1
     )
   end
+  let(:edge_2_for_child_for_child_for_root) do
+    MetaStruct::Graph::Edge.create(
+      left_node: child_for_root,
+      right_node: child_2_for_child_for_root,
+      weight: 2
+    )
+  end
+  let(:edge_3_for_child_for_child_for_root) do
+    MetaStruct::Graph::Edge.create(
+      left_node: child_for_root,
+      right_node: child_3_for_child_for_root,
+      weight: 1
+    )
+  end
+  let(:edge_third_level_for_child_for_child_for_root) do
+    MetaStruct::Graph::Edge.create(
+      left_node: child_3_for_child_for_root,
+      right_node: child_for_child_3_for_child_for_root,
+      weight: 1
+    )
+  end
 
-  let(:nodes) { [child_for_root, root, another_child_for_root, child_for_child_for_root] }
-  let(:edges) { [edge_for_root, another_edge_for_root, edge_for_child_for_child_for_root] }
+  let(:nodes) do
+    [
+      child_for_root,
+      root,
+      another_child_for_root,
+      child_for_child_for_root,
+      child_2_for_child_for_root,
+      child_3_for_child_for_root,
+      child_for_child_3_for_child_for_root
+    ]
+  end
+  let(:edges) do
+    [
+      edge_for_root,
+      another_edge_for_root,
+      edge_for_child_for_child_for_root,
+      edge_2_for_child_for_child_for_root,
+      edge_3_for_child_for_child_for_root,
+      edge_third_level_for_child_for_child_for_root
+    ]
+  end
 
   let(:graph) { MetaStruct::Graph.create(nodes: nodes, edges: edges) }
 
@@ -76,13 +125,73 @@ RSpec.describe MetaStruct::Graph::Algorithms::PointsIterator do
             { uuid: another_child_for_root.uuid, edge: another_edge_for_root },
             { uuid: child_for_root.uuid, edge: edge_for_root },
             {
-              uuid: child_for_child_for_root.uuid,
-              edge: edge_for_child_for_child_for_root
+              uuid: child_2_for_child_for_root.uuid,
+              edge: edge_2_for_child_for_child_for_root
             }
           ]
         end
 
         it { is_expected.to match(expected_result) }
+
+        context 'when iterator always return false' do
+          let(:iterator) do
+            lambda do |point, adjacency|
+              flow << { uuid: point.uuid, edge: adjacency&.edge }
+              false
+            end
+          end
+          let(:expected_result) do
+            [
+              { uuid: root.uuid, edge: nil },
+              { uuid: another_child_for_root.uuid, edge: another_edge_for_root },
+              { uuid: child_for_root.uuid, edge: edge_for_root }
+            ]
+          end
+
+          it { is_expected.to match(expected_result) }
+        end
+
+        context 'when iteration by bound nodes returns false' do
+          let(:bound_node_uuids) do
+            [
+              child_for_child_3_for_child_for_root.uuid,
+              child_2_for_child_for_root.uuid,
+              child_for_child_for_root.uuid,
+              another_child_for_root.uuid
+            ]
+          end
+          let(:iterator) do
+            lambda do |point, adjacency|
+              flow << { uuid: point.uuid, edge: adjacency&.edge }
+              !bound_node_uuids.include?(point.uuid)
+            end
+          end
+          let(:expected_result) do
+            [
+              { uuid: root.uuid, edge: nil },
+              { uuid: another_child_for_root.uuid, edge: another_edge_for_root },
+              { uuid: child_for_root.uuid, edge: edge_for_root },
+              {
+                uuid: child_2_for_child_for_root.uuid,
+                edge: edge_2_for_child_for_child_for_root
+              },
+              {
+                uuid: child_3_for_child_for_root.uuid,
+                edge: edge_3_for_child_for_child_for_root
+              },
+              {
+                uuid: child_for_child_3_for_child_for_root.uuid,
+                edge: edge_third_level_for_child_for_child_for_root
+              },
+              {
+                uuid: child_for_child_for_root.uuid,
+                edge: edge_for_child_for_child_for_root
+              }
+            ]
+          end
+
+          it { is_expected.to match(expected_result) }
+        end
       end
     end
 
@@ -92,8 +201,8 @@ RSpec.describe MetaStruct::Graph::Algorithms::PointsIterator do
         [
           { uuid: child_for_root.uuid, edge: edge_for_root },
           {
-            uuid: child_for_child_for_root.uuid,
-            edge: edge_for_child_for_child_for_root
+            uuid: child_2_for_child_for_root.uuid,
+            edge: edge_2_for_child_for_child_for_root
           }
         ]
       end
